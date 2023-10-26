5587709ef7bbf49308ea943e9798c459f74646d3
IJNikhil <nikhileshwar41@gmail.com>
2023-10-26 22:32:28 +0530

    changes

--- app/src/main/java/com/loop/app/LoginOtpActivity.java
+++ app/src/main/java/com/loop/app/LoginOtpActivity.java
@@ -16,8 +16,11 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthOptions;
 import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.loop.app.utils.AndroidUtil;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
@@ -40,12 +43,15 @@ public class LoginOtpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_otp);
 
+        phoneNumber = getIntent().getExtras().getString("phone");
+
         otpInput = findViewById(R.id.login_otp);
         nextBtn = findViewById(R.id.login_next_btn);
         resendOtpTextView = findViewById(R.id.resend_otp_textview);
         progressBar = findViewById(R.id.login_progress_bar);
 
-        phoneNumber = getIntent().getExtras().getString("phone");
+        AndroidUtil.showToast(getApplicationContext(), phoneNumber);
+
 
         sendOtp(phoneNumber, false);
 
@@ -63,9 +69,12 @@ public class LoginOtpActivity extends AppCompatActivity {
         resendOtpTextView.setOnClickListener(view -> sendOtp(phoneNumber, true));
     }
 
-    void sendOtp(String phoneNumber, boolean isResend) {
+    public void sendOtp(String phoneNumber, boolean isResend) {
+        // Set the progress bar to indicate that the OTP is being sent.
         resendOtpTextView.setText("OTP is Sending..");
         setInProgress(true);
+
+        // Create a PhoneAuthOptions object with the specified phone number, timeout, and activity.
         PhoneAuthOptions.Builder builder = PhoneAuthOptions.newBuilder(mAuth)
                 .setPhoneNumber(phoneNumber)
                 .setTimeout(timeoutSeconds, TimeUnit.SECONDS)
@@ -73,36 +82,51 @@ public class LoginOtpActivity extends AppCompatActivity {
                 .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
                     @Override
                     public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                        // Sign the user in with the phone auth credential.
                         signIn(phoneAuthCredential);
+
+                        // Set the progress bar to indicate that the operation is complete.
                         setInProgress(false);
                     }
 
                     @Override
                     public void onVerificationFailed(@NonNull FirebaseException e) {
+                        // Show a toast message indicating that the OTP was not sent.
                         AndroidUtil.showToast(getApplicationContext(), "OTP not sent");
+
+                        // Set the progress bar to indicate that the operation is complete.
                         setInProgress(false);
 
+                        // Start the login phone number activity.
                         Intent intent = new Intent(LoginOtpActivity.this, LoginPhoneNumberActivity.class);
                         startActivity(intent);
                     }
 
                     @Override
                     public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
-                        super.onCodeSent(s, forceResendingToken);
+                        // Start the resend timer.
                         startResendTimer();
+
+                        // Save the verification code and force resending token.
                         verificationCode = s;
                         resendingToken = forceResendingToken;
+
+                        // Show a toast message indicating that the OTP was sent successfully.
                         AndroidUtil.showToast(getApplicationContext(), "OTP Sent Successfully");
+
+                        // Set the progress bar to indicate that the operation is complete.
                         setInProgress(false);
                     }
                 });
 
+        // If the OTP is being resent, use the force resending token. Otherwise, don't use it.
         if (isResend) {
             PhoneAuthProvider.verifyPhoneNumber(builder.setForceResendingToken(resendingToken).build());
         } else {
             PhoneAuthProvider.verifyPhoneNumber(builder.build());
         }
     }
+
 
     void setInProgress(boolean inProgress) {
         if (inProgress) {
